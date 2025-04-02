64cd4a3a652483e13ccf3ce1756d28296e6273b1
mornir <jeromepott@hotmail.com>
2025-04-02 14:33:12 +0200

    refactor

--- src/utils/sanity.ts
+++ src/utils/sanity.ts
@@ -3,20 +3,6 @@ import type { PortableTextBlock } from "@portabletext/types";
 import type { ImageAsset, Slug } from "@sanity/types";
 import groq from "groq";
 
-export async function getPosts(): Promise<Post[]> {
-  return await sanityClient.fetch(
-    groq`*[_type == "post" && defined(slug.current)] | order(_createdAt desc)`
-  );
-}
-
-export async function getPost(slug: string): Promise<Post> {
-  return await sanityClient.fetch(
-    groq`*[_type == "post" && slug.current == $slug][0]`,
-    {
-      slug,
-    }
-  );
-}
 
 export async function getArticles(): Promise<Article[]> {
   return await sanityClient.fetch(
@@ -24,37 +10,26 @@ export async function getArticles(): Promise<Article[]> {
   );
 }
 
+export declare const internalGroqTypeReferenceTo: unique symbol
 
-export interface Post {
-  _type: "post";
-  _createdAt: string;
-  title?: string;
-  slug: Slug;
-  excerpt?: string;
-  mainImage?: ImageAsset & { alt?: string };
-  body: PortableTextBlock[];
-}
-
-export type BlockContent = Array<
-  | {
-    children?: Array<{
-      marks?: Array<string>
-      text?: string
-      _type: 'span'
-      _key: string
-    }>
-    style?: 'normal' | 'h1' | 'h2' | 'h3' | 'h4' | 'blockquote'
-    listItem?: 'number' | 'bullet'
-    markDefs?: Array<{
-      href?: string
-      _type: 'link'
-      _key: string
-    }>
-    level?: number
-    _type: 'block'
+export type BlockContent = Array<{
+  children?: Array<{
+    marks?: Array<string>
+    text?: string
+    _type: 'span'
     _key: string
-  }
->
+  }>
+  style?: 'normal'
+  listItem?: 'number' | 'bullet'
+  markDefs?: Array<{
+    href?: string
+    _type: 'link'
+    _key: string
+  }>
+  level?: number
+  _type: 'block'
+  _key: string
+}>
 
 export type Article = {
   _id: string
@@ -63,15 +38,87 @@ export type Article = {
   _updatedAt: string
   _rev: string
   number?: number
-  content: {
-    deTitle: string
-    frTitle: string
-    itTitle: string
-    deLaw: BlockContent
-    frLaw: BlockContent
-    itLaw: BlockContent
-    deExplanation?: BlockContent
-    frExplanation?: BlockContent
-    itExplanation?: BlockContent
+  title?: {
+    _ref: string
+    _type: 'reference'
+    _weak?: boolean
+    [internalGroqTypeReferenceTo]?: 'title'
   }
+  chapter?: {
+    _ref: string
+    _type: 'reference'
+    _weak?: boolean
+    [internalGroqTypeReferenceTo]?: 'chapter'
+  }
+  section?: {
+    _ref: string
+    _type: 'reference'
+    _weak?: boolean
+    [internalGroqTypeReferenceTo]?: 'section'
+  }
+  name: LocaleString
+  law: LocaleBlockContent
+  exp?: LocaleBlockContent
+}
+
+export type LocaleBlockContent = {
+  _type: 'localeBlockContent'
+  de?: BlockContent
+  fr?: BlockContent
+  it?: BlockContent
+}
+
+export type Section = {
+  _id: string
+  _type: 'section'
+  _createdAt: string
+  _updatedAt: string
+  _rev: string
+  number?: number
+  title?: {
+    _ref: string
+    _type: 'reference'
+    _weak?: boolean
+    [internalGroqTypeReferenceTo]?: 'title'
+  }
+  chapter?: {
+    _ref: string
+    _type: 'reference'
+    _weak?: boolean
+    [internalGroqTypeReferenceTo]?: 'chapter'
+  }
+  name?: LocaleString
+}
+
+export type Chapter = {
+  _id: string
+  _type: 'chapter'
+  _createdAt: string
+  _updatedAt: string
+  _rev: string
+  number?: number
+  title?: {
+    _ref: string
+    _type: 'reference'
+    _weak?: boolean
+    [internalGroqTypeReferenceTo]?: 'title'
+  }
+  name?: LocaleString
+}
+
+export type Title = {
+  _id: string
+  _type: 'title'
+  _createdAt: string
+  _updatedAt: string
+  _rev: string
+  number?: number
+  name?: LocaleString
+}
+
+export type LocaleString = {
+  _type: 'localeString'
+  de?: string
+  fr?: string
+  it?: string
 }
