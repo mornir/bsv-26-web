9762cee21d4b33091c8f90cb90972b8f12aea82d
mornir <jeromepott@hotmail.com>
2025-04-09 13:49:02 +0200

    refactor: variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,27 @@
+import { loadEnv } from 'vite'
+
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 import sanity from '@sanity/astro'
 import alpinejs from '@astrojs/alpinejs'
 
+const { PUBLIC_SANITY_PROJECT_ID, PUBLIC_SANITY_DATASET } = loadEnv(
+  import.meta.env.MODE,
+  process.cwd(),
+  ''
+)
+import { defineConfig } from 'astro/config'
+
+// Different environments use different variables
+const projectId = PUBLIC_SANITY_PROJECT_ID
+const dataset = PUBLIC_SANITY_DATASET
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     sanity({
-      projectId: 'lc9446ox',
-      dataset: 'production',
+      projectId,
+      dataset,
       useCdn: false,
       apiVersion: '2024-12-08',
     }),
--- netlify/functions/chatbot.mts
+++ netlify/functions/chatbot.mts
@@ -1,5 +1,7 @@
 import type { Context } from "@netlify/functions";
 
+/* import { chatWithSanity } from './utils/sanityChatbot'; */
+
 export default async (req: Request, context: Context) => {
-  return new Response("Hello, world!")
+  return new Response(`hello project id ${process.env.PUBLIC_SANITY_PROJECT_ID}`)
 }
