b2c4535a1562bf62b9b031ce79ac70cd5a67c1b5
mornir <jeromepott@hotmail.com>
2025-05-13 16:27:15 +0200

    update: also use trailingslash on dev

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 import tailwindcss from '@tailwindcss/vite'
-import {defineConfig} from 'astro/config'
+import { defineConfig } from 'astro/config'
 import alpinejs from '@astrojs/alpinejs'
 import icon from 'astro-icon'
 
-import {defineConfig} from 'astro/config'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://bsv.terminofeu.ch',
-  integrations: [alpinejs({entrypoint: '/src/utils/alpine'}), icon()],
+  trailingSlash: 'always',
+  integrations: [alpinejs({ entrypoint: '/src/utils/alpine' }), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
