dd4719fa512148c5f4b16b39347c86f8dd696f2b
mornir <jeromepott@hotmail.com>
2025-07-05 12:07:05 +0200

    wip: i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+  de: 'Deutsch',
+  fr: 'Français',
+  it: 'italiano',
+}
+
+export const defaultLang = 'de'
+
+export const ui = {
+  de: {
+    'nav.home': 'Home',
+    'nav.about': 'About',
+  },
+  fr: {
+    'nav.home': 'Accueil',
+    'nav.about': 'À propos',
+  },
+} as const
