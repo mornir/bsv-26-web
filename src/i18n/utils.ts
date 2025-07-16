99ed6f450e29ef2d55e11095452ce21f5863dd2c
mornir <jeromepott@hotmail.com>
2025-07-16 13:45:45 +0200

    refactor: use Astro.currentLocale

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,13 +1,6 @@
 import { ui, defaultLang, languages } from './ui'
 import type { LocaleString } from '@/sanity/sanity.types'
 
-// TODO: review this function
-export function getLangFromUrl(url: URL) {
-  const lang = url.pathname.split('/')[1]
-  if (lang in ui) return lang as keyof typeof ui
-  return defaultLang
-}
-
 // TODO: currently unused
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof (typeof ui)[typeof defaultLang]) {
