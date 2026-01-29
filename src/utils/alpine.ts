e07ea109502936a431c51241cb271e81ebe85c41
mornir <jeromepott@hotmail.com>
2026-01-29 11:57:25 +0100

    feat: accordion for exp closes

--- src/utils/alpine.ts
+++ src/utils/alpine.ts
@@ -1,11 +1,13 @@
 import type { Alpine } from 'alpinejs'
 import focus from '@alpinejs/focus'
 import persist from '@alpinejs/persist'
+import collapse from '@alpinejs/collapse'
 import tippy from 'tippy.js'
 
 export default (Alpine: Alpine) => {
   Alpine.plugin(focus)
   Alpine.plugin(persist)
+  Alpine.plugin(collapse)
 
   Alpine.directive('tooltip', (el) => {
     tippy(el, { content: el.dataset.message })
