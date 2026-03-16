bd60a77eaca25f5c1abb2748057567c54175c7ec
mornir <jeromepott@hotmail.com>
2026-03-16 16:50:30 +0100

    feat: use meta.import feature

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,8 +1,12 @@
 import { createClient } from '@sanity/client'
 
+const projectId =
+  import.meta.env.SANITY_STUDIO_PROJECT_ID || '<your project ID>'
+const dataset = import.meta.env.SANITY_STUDIO_DATASET || 'production'
+
 export default createClient({
-  projectId: 'lc9446ox',
-  dataset: 'production',
+  projectId,
+  dataset,
   useCdn: false,
   apiVersion: '2026-01-18',
 })
