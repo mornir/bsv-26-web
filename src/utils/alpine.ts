88e9a562355ca73988a256c14eb4727db6988c2a
mornir <jeromepott@hotmail.com>
2026-06-24 16:41:05 +0200

    feat: alpine intersect plugin

--- src/utils/alpine.ts
+++ src/utils/alpine.ts
@@ -3,11 +3,13 @@ import focus from '@alpinejs/focus'
 import persist from '@alpinejs/persist'
 import collapse from '@alpinejs/collapse'
 import tippy from 'tippy.js'
+import intersect from '@alpinejs/intersect'
 
 export default (Alpine: Alpine) => {
   Alpine.plugin(focus)
   Alpine.plugin(persist)
   Alpine.plugin(collapse)
+  Alpine.plugin(intersect)
 
   Alpine.directive('tooltip', (el) => {
     tippy(el, { content: el.dataset.message })
