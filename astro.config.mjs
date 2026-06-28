3d41cacc0cc573b7d168055c8bf7c361d02e5865
mornir <jeromepott@hotmail.com>
2026-06-28 15:08:34 +0200

    fix: include '@alpinejs/intersect' in Vite optimizeDeps

--- astro.config.mjs
+++ astro.config.mjs
@@ -51,6 +51,7 @@ export default defineConfig({
         '@alpinejs/focus',
         '@alpinejs/persist',
         '@alpinejs/collapse',
+        '@alpinejs/intersect',
         'tippy.js',
       ],
     },
